a18b98aa4d710af93613d7580ef04248004e3862
simonholt-flyinglizard <simonvholt92@gmail.com>
2026-05-14 17:03:47 +0100

    Re-Design
    Total colour scheme and layout redesign

--- portfolio/src/pages/toppages/home.jsx
+++ portfolio/src/pages/toppages/home.jsx
@@ -12,43 +12,75 @@ import { motion } from "framer-motion";
 function Home() {
   return (
     <>
-      <h1 className="home-title">Welcome</h1>
-      <div className="pic-small-box">
-        <img className="profile-pic-small" src={Profilepic}/>
-      </div>
-      <div className='tech-divider-home'>
-        <div className="tech-divider-home__text">
-          Tech Used: HTML - CSS - JavaScript - React js - Python
-        </div>
+    <div className="tech-block">python etc</div>
+    <div className="intro-box">
+      
+      <div className="tech-info-box">
+        <div className="intro-title-box">Hi, I'm Simon,<br></br> what can I do for you?</div>
+        
+          <div className="intro-text-box">
+            I’m a self taught full-stack software developer looking
+            for an opportunity to start my career in the field.
+            <br></br>
+            <br></br>
+            Having an understanding of all the working parts that create a fully
+            functioning application means that whichever area I am focusing my attention
+            on, I will always keep in mind the big pictures and how that specific element
+            will interact with those around it.
+            <div className="orange-border"></div>
+          </div>
+
         
       </div>
-      <div className='chrome-box-outer intro-box'>
-        <div className='chrome-box-inner'>
-          Hello, I’m Simon. I’m a self taught full-stack software developer looking
-          for an opportunity to start my career in the field.
-          <br></br>
-          <br></br>
-          Having an understanding of all the working parts that create a fully
-          functioning application means that whichever area I am focusing my attention
-          on, I will always keep in mind the big pictures and how that specific element
-          will interact with those around it.
+      <div className="profile-picture-box">
+        <div className="yellow-pattern">
+          <img className="profile-pic" src={Profilepic}/>
         </div>
       </div>
+    </div>
+    <div className="phone-intro-box">
+      <div className="phone-tech-block">python etc</div>
+      <div className="phone-title">Hi, I'm Simon,<br></br> what can I do for you?</div>
+      <div className="phone-profile-picture-box">
+        <div className="yellow-pattern">
+          <img className="profile-pic" src={Profilepic}/>
+        </div>
+      </div>
+      <div className="phone-text">
+        I’m a self taught full-stack software developer looking
+        for an opportunity to start my career in the field.
+        <br></br>
+        <br></br>
+        Having an understanding of all the working parts that create a fully
+        functioning application means that whichever area I am focusing my attention
+        on, I will always keep in mind the big pictures and how that specific element
+        will interact with those around it.
+      </div>
+    </div>
       
-      <div className='orange-divider-home'>
-        <img className="guy-pic" src={Profilepic}/>
+      <div className='divider-home'>
+      </div>
+
+      <div className="view-projects-box">
+        <div className="view-projects-display">
+          <button className="view-projects-button">View projects!</button>
+        </div>
       </div>
       <div className="key-principles-text">
-        There are a few key principles I think are vital for a project to reach its best form. Communication. Maintainability. Creativity.
+        There are a few key principles I think are vital for a project to reach its best form.
+      </div>
+      <div className="principle-anchor-box">
+        <div className="principle-anchor-box__item"><a>Commincation</a></div>
+        <div className="principle-anchor-box__item"><a>Maintainability</a></div>
+        <div className="principle-anchor-box__item"><a>Creativity</a></div>
       </div>
       <motion.div
-        className='chrome-box-outer intro-box'
+        className='text-card'
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true, amount: 0.2 }}
       >
-        <div className="chrome-box-inner">
           <h3>Communication</h3>
           <div className="communication-box__text-icon-split">
             <p>
@@ -60,51 +92,43 @@ function Home() {
             </p>
             <img className="comm-icon" src={Chat}></img>
           </div>
-          
-        </div>
       </motion.div>
       <div className="blue-divider-home"></div>
       <motion.div
-        className='chrome-box-outer intro-box'
+        className='text-card'
         initial={{ opacity: 0, x: 100 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true, amount: 0.2 }}
-      >
-        <div className="chrome-box-inner">
-          <h3>Maintainability</h3>
-          <div className="maintain-box__text-icon-split">
-            <p>
-              Readable code that is understood at a glance, helps work done today and in the future.
-              Conscious and deliberate decisions about system architecture allow for potential scaling
-              and reduce headaches when changes inevitably have to be made. Clear and precise notes are
-              a must. Be kind to your future self (or those who come after you). 
-            </p>
-            <img className="comm-icon" src={Setting}></img>
-          </div>
-          
+      >  
+        <h3>Maintainability</h3>
+        <div className="maintain-box__text-icon-split">
+          <p>
+            Readable code that is understood at a glance, helps work done today and in the future.
+            Conscious and deliberate decisions about system architecture allow for potential scaling
+            and reduce headaches when changes inevitably have to be made. Clear and precise notes are
+            a must. Be kind to your future self (or those who come after you). 
+          </p>
+          <img className="comm-icon" src={Setting}></img>
         </div>
       </motion.div>
       <div className="yellow-divider-home"></div>
       <motion.div
-        className='chrome-box-outer intro-box'
+        className='text-card'
         initial={{ opacity: 0, x: -100 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true, amount: 0.2 }}
       >
-        <div className="chrome-box-inner">
-          <h3>Creativity</h3>
-          <div className="creative-box__text-icon-split">
-            <p>
-              Creative thinking creates solutions. Whether that happens through tailoring an algorithm to
-              sort data in the most efficient way, or through finding a new design style that captures a user's
-              imagination in a unique way. It is creativity and the passion it fuels that gets good work done.
-              Used well, creativity is what shifts the ordinary from the mundane to the spectacular.
-            </p>
-            <img className="comm-icon" src={Idea}></img>
-          </div>
-          
+        <h3>Creativity</h3>
+        <div className="creative-box__text-icon-split">
+          <p>
+            Creative thinking creates solutions. Whether that happens through tailoring an algorithm to
+            sort data in the most efficient way, or through finding a new design style that captures a user's
+            imagination in a unique way. It is creativity and the passion it fuels that gets good work done.
+            Used well, creativity is what shifts the ordinary from the mundane to the spectacular.
+          </p>
+          <img className="comm-icon" src={Idea}></img>
         </div>
       </motion.div>
     </>
